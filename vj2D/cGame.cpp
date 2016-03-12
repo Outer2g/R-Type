94e6b5e1bdad5e2394effb5d07e263b8ee3f24c6
Dani <dani_rl94@hotmail.com>
2016-03-12 11:13:35 +0100

    update

--- vj2D/cGame.cpp
+++ vj2D/cGame.cpp
@@ -56,7 +56,7 @@ bool cGame::Loop()
 	double t1, t2;
 
 	t1 = glutGet(GLUT_ELAPSED_TIME);
-	++offsetCamera;
+	if (offsetCamera < Scene.BACK_WIDTH_DRAW - GAME_WIDTH) ++offsetCamera;
 	
 	res = Process();
 	if(res) Render();
